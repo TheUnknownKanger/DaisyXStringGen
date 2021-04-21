5523accf68f047830ad9a32d2bc600ac4fae33e6
The Unknown <77438385+TheUnknownKanger@users.noreply.github.com>
2021-04-21 11:03:54 +0530

    Update StringGen.py

--- StringGen.py
+++ StringGen.py
@@ -179,3 +179,44 @@ async def is_cancel(msg: Message, text: str):
 
 if __name__ == "__main__":
     bot.run()
+
+from pyrogram import Client
+from pyrogram.filters import command, other_filters, other_filters2
+
+
+@Client.on_message(command("start") & other_filters2)
+async def start(_, message: Message):
+    await message.reply_text(
+        f"""🙃 Hi {message.from_user.first_name}!
+✨ Hey I am DaisyX String Generator Bot. 
+🥳 I can Generator String Session for You 😉
+⚜️ Use these buttons below to know more. 👇
+🔥 Source Code Made by Devil With Help Of Other Bots 🔥
+👉 Type /genstr for Generating String Session 👈""",
+        reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        "⚒ Source code", url="https://github.com/SkemTools"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "💬 Group", url="https://t.me/DaisySupport_Official"
+                    ),
+                    InlineKeyboardButton(
+                        "Channel 🔈", url="https://t.me/DaisyXUpdates"
+                    )
+                ],
+                [
+                    InlineKeyboardButton(
+                        "❌ Close ❌", callback_data="close"
+                    )
+                ]
+            ]
+        )
+    )
+
+
+
+
