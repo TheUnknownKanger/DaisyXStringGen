e4817cc147df87509c95efc23c3c969466753895
The Unknown <77438385+TheUnknownKanger@users.noreply.github.com>
2021-04-21 11:17:55 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,18 +11,18 @@ from pyrogram.errors import MessageNotModified
 
 
 class Config:
-    APP_ID = int(os.environ.get("APP_ID", 0))
+    API_ID = int(os.environ.get("API_ID", 0))
     API_HASH = os.environ.get("API_HASH", None)
     BOT_TOKEN = os.environ.get("BOT_TOKEN", None)
     APP_NAME = os.environ.get("APP_NAME", None)
     API_KEY = os.environ.get("API_KEY", None)
     HU_APP = from_key(API_KEY).apps()[APP_NAME]
-
+    APP_ID = os.environ.get("APP_ID")
 
 class Bot(Client):
     def __init__(self):
         kwargs = {
-            'app_id': Config.APP_ID,
+            'api_id': Config.API_ID,
             'api_hash': Config.API_HASH,
             'session_name': ':memory:',
             'bot_token': Config.BOT_TOKEN
